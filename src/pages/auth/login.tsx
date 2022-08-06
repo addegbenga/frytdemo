27e95c796dce011b939024c2da776e03beab92f3
Adeyemi <addegbenga@gmail.com>
2022-08-06 19:53:16 +0100

    fix: script

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,14 +1,33 @@
-// import Login from '@components/auth/login/login';
-import dynamic from 'next/dynamic';
-const Login = dynamic(() => import('../../components/auth/login/login'), {
-  ssr: false,
-  loading: () => <div>Loading</div>,
-});
+// // import Login from '@components/auth/login/login';
+// import dynamic from 'next/dynamic';
+// const Login = dynamic(() => import('../../components/auth/login/login'), {
+//   ssr: false,
+//   loading: () => <div>Loading</div>,
+// });
 
-export default function LoginView() {
+// export default function LoginView() {
+//   return (
+//     <div>
+//       <Login />
+//     </div>
+//   );
+// }
+
+import React from 'react';
+// import Register from '@components/auth/Register/Register';
+import dynamic from 'next/dynamic';
+const Register = dynamic(
+  () => import('../../components/auth/Register/Register'),
+  {
+    ssr: false,
+    loading: () => <div>Loading</div>,
+  }
+);
+
+export default function RegisterView() {
   return (
     <div>
-      <Login />
+      <Register />
     </div>
   );
 }
